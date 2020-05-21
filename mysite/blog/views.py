811dc3e9453cc01399d92c185d5ab576cd3d2310
Avaaren <avaaren@mail.ru>
2020-05-21 09:02:45 +0300

    Removed excess view func

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -66,26 +66,6 @@ def post_detail(request, year, month, day, post):
                                                      }
                   )
 
-
-def post_search(request):
-    form = SearchForm()
-    query = None
-    results = []
-    if 'query' in request.GET:
-        form = SearchForm(request.GET)
-    if form.is_valid():
-        query = form.cleaned_data['query']
-        results = Post.objects.annotate(search=SearchVector('title','body').filter(search=query))
-        return render(
-            request, 'blog/post/search.html', {
-                'form': form,
-                'query': query,
-                'results': results
-            }
-        )
-
-            
-
 def register(request):
     if request.method == "POST":
         user_form = UserForm(request.POST, request.FILES)
